7261094fcb407989bd6b6e0c6dd3b28b2c7ec038
John K Hermann <john.hermann@webstep.no>
2022-10-14 12:24:30 +0200

    latlng API work in progress

--- index.js
+++ index.js
@@ -11,12 +11,35 @@ app.get('/', (req, res) => {
 })
 
 // API - vannmiljodatabase - havforsuringsdata
+app.get('/v1/waterlocationlatlongs', async (req, res) => {
+    // PSEUDO code
+    // steg 1: kalle på /v1/havforsuringsdata API'et nedenfor
+    // steg 2: sortere på unike WaterLocationID's (liste med ID'er)
+    // steg 3: loope igjennom listen med ID'er og gjøre oppslag mot arcGIS serveren:
+    const baseUrl = "https://arcgis06.miljodirektoratet.no/arcgis/rest/services/vannmiljo/vannmiljo/MapServer/1/query?"
+    const waterLocationID = "38280"
+    const queryParams = `where=waterlocationid+%3D+${waterLocationID}&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&havingClause=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&datumTransformation=&parameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=pjson`
+
+    res.send([])
+
+    // returns a list with the following format:
+    //     [
+    //     { waterLocationID: 12344, lat: 64.312312, lng: 11.0000123},
+    //     { waterLocationID: 12345, lat: 64.312312, lng: 11.0000123},
+    //     { waterLocationID: 12346, lat: 64.312312, lng: 11.0000123},
+    //     { waterLocationID: 12347, lat: 64.312312, lng: 11.0000123},
+    //     { waterLocationID: 12348, lat: 64.312312, lng: 11.0000123},
+    //     { waterLocationID: 12349, lat: 64.312312, lng: 11.0000123},
+    // ]
+})
+
+// API - vannmiljodatabase - havforsuringsdata
 app.get('/v1/havforsuringsdata', async (req, res) => {
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     
     let urlencoded = new URLSearchParams();
-    urlencoded.append("MaxReturnCount", "10");
+    urlencoded.append("MaxReturnCount", "66000");
     urlencoded.append("Username", "vannmiljowebapi");
     urlencoded.append("Password", "vanntemppw");
     urlencoded.append("ActivityID", "HAVF");
